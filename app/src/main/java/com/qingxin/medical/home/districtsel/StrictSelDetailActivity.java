6f1cd583a580ae334e393bcdcf64d80307374938
youzhiqingnian <359455763@qq.com>
2018-02-08 18:13:34 +0800

    增加了严选详情

---
+++ app/src/main/java/com/qingxin/medical/home/districtsel/StrictSelDetailActivity.java
@@ -0,0 +1,23 @@
+package com.qingxin.medical.home.districtsel;
+
+import android.os.Bundle;
+
+import com.qingxin.medical.R;
+import com.qingxin.medical.base.QingXinActivity;
+
+/**
+ * 严选详情界面
+ * Date 2018/2/8
+ *
+ * @author zhikuo
+ */
+public class StrictSelDetailActivity extends QingXinActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_local_strictsel);
+    }
+}
