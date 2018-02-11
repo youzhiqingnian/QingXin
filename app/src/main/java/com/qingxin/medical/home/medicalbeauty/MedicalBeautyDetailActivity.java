41d254d3137d0b7fd4a248e286abbc2fb5d7d5f4
chenjiaxing <359455763@qq.com>
2018-02-11 14:42:47 +0800

    yimeibaike

---
+++ app/src/main/java/com/qingxin/medical/home/medicalbeauty/MedicalBeautyDetailActivity.java
@@ -0,0 +1,22 @@
+package com.qingxin.medical.home.medicalbeauty;
+
+import android.os.Bundle;
+
+import com.qingxin.medical.R;
+import com.qingxin.medical.base.QingXinActivity;
+import com.qingxin.medical.base.QingXinApplication;
+
+/**
+ * Date 2018-02-11
+ *
+ * @author zhikuo1
+ */
+
+public class MedicalBeautyDetailActivity extends QingXinActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medical_beauty_detail);
+    }
+}
