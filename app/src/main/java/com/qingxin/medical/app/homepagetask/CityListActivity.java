b8df7c1e26425de71844e503f4e60e538b63b492
chenjiaxing <359455763@qq.com>
2018-03-06 20:35:07 +0800

    city pre

---
+++ app/src/main/java/com/qingxin/medical/app/homepagetask/CityListActivity.java
@@ -0,0 +1,23 @@
+package com.qingxin.medical.app.homepagetask;
+
+import android.os.Bundle;
+
+import com.qingxin.medical.base.QingXinActivity;
+
+/**
+ * Date 2018-03-06
+ *
+ * @author zhikuo1
+ */
+
+public class CityListActivity  extends QingXinActivity {
+
+    public static void startSelf(){
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
