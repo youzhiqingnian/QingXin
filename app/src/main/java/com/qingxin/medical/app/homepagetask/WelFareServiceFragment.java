cd07a91d4aabd1a9ee36b5b98fd39ebd153af477
chenjiaxing <359455763@qq.com>
2018-03-06 09:40:13 +0800

    what what a

--- app/src/main/java/com/qingxin/medical/app/homepagetask/WelFareServiceFragment.java
+++ app/src/main/java/com/qingxin/medical/app/homepagetask/WelFareServiceFragment.java
@@ -255,7 +255,7 @@ public class WelFareServiceFragment extends VLFragment implements WelfareCoinLog
             case R.id.applyWithDrawalsTv:
                 // 申请提现
                 if (Long.valueOf(QingXinApplication.getInstance().getLoginUser().getCoin()) >= 10000) {
-                    applyWithdrawalsDialog.setBalance(mQingxinCoinAmountTv.getText().toString().trim(), VLUtils.keepTwoSecimal2(Double.valueOf((Long.valueOf(mQingxinCoinAmountTv.getText().toString().trim()) / 100)) + ""));
+                    applyWithdrawalsDialog.setBalance(mQingxinCoinAmountTv.getText().toString().trim(), QingXinApplication.getInstance().getLoginSession().getMem().getAvailable_coin());
                     applyWithdrawalsDialog.show();
                 } else {
                     VLDialog.showAlertDialog(getActivity(), getActivity().getResources().getString(R.string.warm_remind), getActivity().getResources().getString(R.string.can_not_withdrawals_remind_message), true, new VLResHandler() {
