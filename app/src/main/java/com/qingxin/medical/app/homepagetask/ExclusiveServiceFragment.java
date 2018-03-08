f145257d5a30af257321fd0da1fbdb36e6b8888a
chenjiaxing <359455763@qq.com>
2018-03-08 11:52:28 +0800

    margin

--- app/src/main/java/com/qingxin/medical/app/homepagetask/ExclusiveServiceFragment.java
+++ app/src/main/java/com/qingxin/medical/app/homepagetask/ExclusiveServiceFragment.java
@@ -55,7 +55,7 @@ public class ExclusiveServiceFragment extends VLFragment implements ServiceListC
 
     @Override
     public View onCreateContent(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_goddess_diary_list, container, false);
+        return inflater.inflate(R.layout.fragment_welfareservice, container, false);
     }
 
     @Override
